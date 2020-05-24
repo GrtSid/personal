239f9802949e95e72bdab58cf548692076b29501
GrtSid <40738512+GrtSid@users.noreply.github.com>
2020-05-25 02:16:32 +0530

    update

--- personal/settings.py
+++ personal/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'django_scss.finders.SCSSFinder',
 ]
 
 ROOT_URLCONF = 'personal.urls'
