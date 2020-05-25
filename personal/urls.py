c0bc1bfb6ff3eb24a632e20a66a3f3d435f3b1c7
grtsid <siddhantagarwal99@gmail.com>
2020-05-26 02:04:47 +0530

    test2

--- personal/urls.py
+++ personal/urls.py
@@ -17,8 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,6 +24,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path(r'^project/', include('info.urls')),
 ]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
