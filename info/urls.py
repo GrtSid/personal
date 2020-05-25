77242d34c01c2f5cffd115027e1cd07eb56ce484
grtsid <siddhantagarwal99@gmail.com>
2020-05-26 01:44:29 +0530

    trial

--- info/urls.py
+++ info/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('', views.Cloth_Classification, name="classification"),
 ]
-
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
