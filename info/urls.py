1f29c7f37f4ff931f3015a44e3eab8e4851c0d4c
grtsid <siddhantagarwal99@gmail.com>
2020-05-26 17:43:06 +0530

    fixed bugs

--- info/urls.py
+++ info/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from . import views
 from django.conf import settings
+from django.urls import path
+from info import views
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-    path(r'^project/', views.Cloth_Classification, name="classification"),
     path('', views.index, name="index"),
+    path(r'project/', views.Cloth_Classification, name="classification"),
+
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
