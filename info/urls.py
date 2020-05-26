b098f91cc19e24b04aec4291a0bc5b5cb6c54b01
grtsid <siddhantagarwal99@gmail.com>
2020-05-26 21:26:35 +0530

    removed model

--- info/urls.py
+++ info/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path(r'project/', views.Cloth_Classification, name="classification"),
+    # path(r'project/', views.Cloth_Classification, name="classification"),
 
 ]
 
--- info/views.py
+++ info/views.py
@@ -9,14 +9,14 @@ def index(request):
     return render(request, 'index.html', context=None)
 
 
-@csrf_exempt
-def Cloth_Classification(request):
-    if request.method == 'POST' and request.FILES['file']:
-        myfile = request.FILES['file']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        value = classify(uploaded_file_url)
-        content = {'url': uploaded_file_url, 'value': value}
-        return JsonResponse(content)
-    return render(request, 'index.html')
+# @csrf_exempt
+# def Cloth_Classification(request):
+#     if request.method == 'POST' and request.FILES['file']:
+#         myfile = request.FILES['file']
+#         fs = FileSystemStorage()
+#         filename = fs.save(myfile.name, myfile)
+#         uploaded_file_url = fs.url(filename)
+#         value = classify(uploaded_file_url)
+#         content = {'url': uploaded_file_url, 'value': value}
+#         return JsonResponse(content)
+#     return render(request, 'index.html')
